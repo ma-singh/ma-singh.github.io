544e4d106a35bb787b9b36d4020fa85c203a93d4
ma-singh <masingh@fullsail.edu>
2017-05-23 20:18:31 -0400

    adjusted major styling

--- js/main.js
+++ js/main.js
@@ -8,13 +8,7 @@
 	project.on('click', function(e){
 		e.preventDefault();
 		console.log('clicked!');
-		var url = $(this).attr('href'); 
+		var url = $(this).attr('href');
     	window.open(url, '_blank');
-	});
-
-	// mouseover text descriptions for projects
-	$('.text').hide().removeClass('text').addClass('text-js');
-	$('.thumb').hover(function(){
-    	$(this).find('.text-js').fadeToggle();
 	});
 })(jQuery);
